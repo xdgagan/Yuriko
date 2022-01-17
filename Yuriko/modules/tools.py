fbdf40f783da538cee992ef2b52101b449f3f386
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2022-01-18 01:58:04 +0530

    Update tools.py

--- Yuriko/modules/tools.py
+++ Yuriko/modules/tools.py
@@ -1,7 +1,7 @@
 __mod_name__ = "Tᴏᴏʟꜱ"
 
 __help__ = """
-*Yuriko Info bot*
+*Miss queen Info bot*
 
 ✗  /sg <reply> - `To check history name`
 
@@ -57,5 +57,4 @@ __help__ = """
 
 ✗  /tagall - `for tag everyone at least 100 member first.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
 """
