300ca25d75c59466ee0ef5c5c8eb8c48d85660a0
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2022-01-18 03:25:32 +0530

    Update Music bot.py

--- Yuriko/modules/Music bot.py
+++ Yuriko/modules/Music bot.py
@@ -1,3 +1,59 @@
 __mod_name__ = "ᴍᴜsɪᴄ"
 
-__help__ = """ """
+__help__ = """ 
+ Admin use command
+
+/pause
+- Pause the playing music on voice chat.
+
+/resume
+- Resume the paused music on voice chat.
+
+/skip
+- Skip the current playing music on voice chat
+
+/end or /stop
+- Stop the playout.
+
+/queue
+- Check queue list.
+
+/activevc
+- Check active voice chats on bot.
+
+/activevideo
+- Check active video calls on bot.
+
+/playplaylist 
+- Start playing Your Saved Playlist.
+
+
+/playlist 
+- Check Your Saved Playlist On Servers.
+
+
+/delmyplaylist
+- Delete any saved music in your playlist
+
+
+/delgroupplaylist
+- Delete any saved music in your group's playlist [Requires Admin Rights.]
+
+Auth users can skip, pause, stop, resume Voice Chats even without Admin Rights.
+
+
+/auth [Username or Reply to a Message] 
+- Add a user to AUTH LIST of the group.
+
+/unauth [Username or Reply to a Message] 
+- Remove a user from AUTH LIST of the group.
+
+/authusers 
+- Check AUTH LIST of the group.
+
+non admin 
+
+/play [Reply to any Video] or [YT Link] or [Music Name]
+- Stream Video on Voice Chat
+
+"""
